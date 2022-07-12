0428cbaca2cd6bd1a45fa3709dddcc8ee6142647
Md. Sujan Ahmed <shobuj2sujon@gmail.com>
2022-07-13 00:56:12 +0600

    Error Page Style

--- src/components/ErrorPage/Error.jsx
+++ src/components/ErrorPage/Error.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Error = () => {
   return (
-    <>
-        <h1>Error 404 - Page Not Found</h1>
-    </>
+    <section className="error">
+        <div className="error__content">
+            <h2><span>Error 404</span> - Page Not Found</h2>
+            <p>The page you are looking for doesn't exist or has been moved.</p>
+        </div>
+    </section>
   )
 }
 
-export default Error
+export default Error;
