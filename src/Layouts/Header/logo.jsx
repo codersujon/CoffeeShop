0ed8632d3d3da34730e1cd12099c47d0fbfe8049
Md. Sujan Ahmed <shobuj2sujon@gmail.com>
2022-07-13 02:59:05 +0600

    styles directory changes

--- src/Layouts/Header/logo.jsx
+++ src/Layouts/Header/logo.jsx
@@ -4,7 +4,7 @@ import Logo from '../../Assets/images/logo.png';
 const logo = (props) => {
     return (
         <div className="logo">
-            <a href="">
+            <a href="/">
                 <img src={Logo} alt="Logo" />
             </a>
         </div>
